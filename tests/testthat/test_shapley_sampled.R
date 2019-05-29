86e0f03866d5329cefe019a75f1aba1b8c1aeb93
Benjamin Elbers <elbersb@gmail.com>
2019-05-29 12:14:59 +0200

    fix tests

--- tests/testthat/test_shapley_sampled.R
+++ tests/testthat/test_shapley_sampled.R
@@ -9,13 +9,11 @@ reg <- function(regressors) {
 }
 
 test_that("regression example", {
-    expect_warning({
-        res1 <- shapley_sampled(reg,
-                c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"),
-                last_n = 10,
-                precision = .01,
-                silent = TRUE)
-    })
+    res1 <- shapley_sampled(reg,
+            c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"),
+            last_n = 10,
+            precision = .01,
+            silent = TRUE)
 
     expect_equal(nrow(res1), 10)
 })
@@ -24,11 +22,10 @@ test_that("regression example", {
     res2 <- shapley(reg,
               list(c("cyl", "disp", "hp", "drat"), c("qsec", "vs", "am", "gear")),
               silent = TRUE)
-    expect_warning({
-        res3 <- shapley_sampled(reg,
-                  list(c("cyl", "disp", "hp", "drat"), c("qsec", "vs", "am", "gear")),
-                  precision = .05,
-                  silent = TRUE)
-    })
+    res3 <- shapley_sampled(reg,
+              list(c("cyl", "disp", "hp", "drat"), c("qsec", "vs", "am", "gear")),
+              precision = .05,
+              silent = TRUE)
+
     expect_lt(mean(exp(abs(log(res2$value / res3$value)))), 1.20)
 })
