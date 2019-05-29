ef2f7ba654650250a7b19b388bbba7c8263182ce
Benjamin Elbers <elbersb@gmail.com>
2019-05-29 11:40:43 +0200

    debug

--- R/shapley.R
+++ R/shapley.R
@@ -276,6 +276,8 @@ shapley_sampled <- function(vfun, factors,
 
     total_diff <- get_from_cache(1:n_factors) - get_from_cache(c())
     print(df)
+    print(total_diff)
+    print(sum(df$value))
     ratio <- abs(log(total_diff / sum(df$value)))
     if (ratio > log(1.01)) {
         pct <- (exp(ratio) - 1) * 100
