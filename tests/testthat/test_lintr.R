6243798f6e8ea6c66031c6a9dadc4ae7b746d534
Benjamin Elbers <elbersb@gmail.com>
2020-02-11 09:45:47 -0500

    remove lintr

--- tests/testthat/test_lintr.R
+++
@@ -1,9 +0,0 @@
-if (requireNamespace("lintr", quietly = TRUE)) {
-    context("lintr")
-
-    test_that("Package Style", {
-
-        lintr::expect_lint_free(linters = lintr::with_defaults(
-            line_length_linter = lintr::line_length_linter(100)))
-    })
-}
